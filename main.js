2d64801e50f894197d4450e3f8055f4d9c36b7a2
Yoon <lvev9925@naver.com>
2013-12-28 17:37:29 +0900

    Circular Rolling 구현완료

--- main.js
+++ main.js
@@ -2,14 +2,6 @@
 function init() {
 	registerEvents();	
 	xhrProcess();
-	
-	
-	var test = document.querySelector(".article_frame");
-	console.log(test);
-	
-	console.log(test.clientWidth);
-	
-	//var test2 = document.querySelector("")
 }
 
 function registerEvents() {
@@ -31,12 +23,7 @@ function scrollFrame(tagId, frames) {
 
 	//global variable이 아닌 객체로 변수값들을 관리한다.
 	var interval = new Object();
-	
-	interval.id = null;
 	interval.direction = null;
-	interval.count = 0;
-	interval.max = 50;
-	interval.isDoing = false;
 	
 	if ( tagId === "rolling_left_btn" ) {
 		//방향에 따라서 direction 상수값을 바꿔준다
@@ -47,61 +34,54 @@ function scrollFrame(tagId, frames) {
 		return;
 	}
 	
+	interval.id = null;
+	interval.count = 0;
+	interval.max = 50;
+	interval.boundary = Math.abs(parseInt(getStyle(frames, 'left'))) + 1000;
 	
+	console.log("interval. boundary");
+	console.log(interval.boundary);
+		
 	
-	var test = document.querySelector(".article_fix_size .wrapper");
+	/*
+var test = document.querySelector(".article_fix_size .wrapper");
 	var style_left = parseInt(getStyle(frames, 'left'));
 	console.log(style_left);
 	
 	frames.style.left = (style_left + (1000*interval.direction)) + "px";
-	/*
+*/
+	
 interval.id = setInterval(
 		function() {
 			//전역변수를 쓸 필요 없이 함수로 전달할 수 있는 방법!!!! 최고!!!
 			( animationMove ) ( interval, frames );	
 		}, 2);
-*/
+
 }
 
 //interval object와 ul의 li리스트 전달
 function animationMove( interval, frames ) {
-	interval.isDoing = true;
-	var style_left = parseInt(getStyle(frames, 'left'));
+	var style_left = parseInt(getStyle(frames, 'left'));	
+	console.log( interval.direction * style_left );	//for test
+	frames.style.left = style_left + (interval.direction * 20) +"px";	
+		++interval.count;
 	
-	console.log( interval.direction * style_left );
-
 	// 10씩 50회(max회) 이동했을 경우 (한번 move할때마다 count가 1씩 증가한다.)	
-	if ( interval.max <= interval.count ) {
+	if ( interval.max < interval.count ) {
 		clearInterval(interval.id);
-		interval.count = 0;
-	}
-	
-	
-	// ul태그가 오른쪽, 혹은 왼쪽으로 li한칸만큼 이동했을 경우 ( ==500일 경우이다 )
-	if ( interval.direction * style_left >= 1276 ) {
-	
-		/*
-		//방향을 확인한 후
+		interval.count = 0;		
+		
 		if ( interval.direction === -1 )
 			//가장 끝쪽에 있는 li엘리먼트를 반대편 끝으로 이동시킨뒤
 			frames.insertBefore(frames.children[0] , frames.children[5]);
 		else
 			frames.insertBefore(frames.children[3] , frames.children[0]);
-		*/
 		
 			
 		//순식간에 ul태그 전체를 li엘리먼트가 이동한 반대편쪽으로 500만큼 이동시킨다. (그럼 view상에서는 변화없이 ul태그의 위치와 순서를 바꿀 수 있다)
-		//frames.style.left = style_left + (interval.direction * -1276)+"px";
-		
-		//1276/50 = 25.52
-		
-		//애니메이션 종료 및 함수 탈출
-		clearInterval(interval.id);
-		return;
+		frames.style.left = style_left + (interval.direction * -1000)+"px";
+
 	}
-	
-	frames.style.left = style_left + (interval.direction * 25.52) +"px";
-	interval.count++;
 }
 
 
